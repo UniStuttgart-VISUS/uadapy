0f08c8c3ca9834052c38ebb5f3563546370bbbb0
David Hägele <haegeldd@visus.uni-stuttgart.de>
2024-01-30 16:20:52 +0100

    distribution class

---
+++ imuncertain/distribution.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy as np
+
+
+class distrib:
+    
+    
+
+    def __init__():
+        self.name = ""
+        self.dim = 1
+
+
+
+    def sample(n: int) -> np.ndarray:
+        ...
+
+
+    def pdf(x: np.ndarray | float) -> np.ndarray | float:
+        ...
+
+
+    def mean() -> np.ndarray | float:
+        ...
+
+    def cov() -> np.ndarray | float:
+        ...
+
+
+    def skew() -> 
