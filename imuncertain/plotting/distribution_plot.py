87083e9936f25639b5257754060de11a5c0bda36
Ruben Bauer <bauer.ruben98@googlemail.com>
2024-01-31 15:17:52 +0100

    intermediate commit: building interactive splom

---
+++ imuncertain/plotting/distribution_plot.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+
+def confidence_ellipse(mean: np.ndarray, cov: np.ndarray, ax: plt.Axes, n_std: float = 3.0,
+                       facecolor='blue', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    assert len(mean) == 2
+    assert cov.shape[-1] == 2
+
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = mean[0]
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = mean[1]
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+class InteractiveNormal:
+    def __init__(self, mean: np.ndarray, cov: np.ndarray, ax: plt.Axes, n_std: float = 1.0, extends: float = 10,
+                 epsilon: float = 10):
+        self.mean = mean
+        self.cov = cov
+        self.ax = ax
+        self.n_std = n_std
+        self.extends = extends
+        self.epsilon = epsilon
+
+        # init points
+        self.points = self.init_points()
+        self.update()
+
+    def init_points(self) -> np.ndarray:
+        eigenvalues, eigenvectors = np.linalg.eig(self.cov)
+        return np.sign(eigenvalues) * np.sqrt(np.abs(eigenvalues)) * eigenvectors
+
+    def update(self):
+        self.ax.clear()
+
+        # plot ellipse
+        confidence_ellipse(self.mean, self.cov, self.ax, self.n_std)
+
+        # plot lines of eigenvectors
+        self.ax.plot([self.mean[0], self.points[0, 0]], [self.mean[1], self.points[0, 1]])
+        self.ax.plot([self.mean[0], self.points[1, 0]], [self.mean[1], self.points[1, 1]])
+
+        # plot points of eigenvectors
+        self.ax.scatter(self.points[:, 0], self.points[:, 1])
+
+        # plot mean center
+        self.ax.scatter(self.mean[0], self.mean[1], c="black")
+
+        extends = self.extends  # 2 * np.abs(points).max()
+
+        self.ax.set_xlim([-extends, extends])
+        self.ax.set_ylim([-extends, extends])
+        self.ax.get_figure().canvas.draw_idle()
+
+    def get_ind_under_point(self, event):
+        'get the index of the vertex under point if within epsilon tolerance'
+        # display coords
+        # print('display x is: {0}; display y is: {1}'.format(event.x,event.y))
+        tinv = self.ax.transData
+        # print('data x is: {0}; data y is: {1}'.format(xy[0],xy[1]))
+        xyt = tinv.transform(self.points.T)
+        xt, yt = xyt[:, 0], xyt[:, 1]
+        d = np.hypot(xt - event.x, yt - event.y)
+        indseq, = np.nonzero(d == d.min())
+        ind = indseq[0]
+
+        if d[ind] >= self.epsilon:
+            ind = None
+
+        return ind
+
+    def adjust_points(self, point_index: int, new_value: np.ndarray):
+        new_point = new_value
+        new_point_length = np.linalg.norm(new_point)
+        points = self.points.copy()
+        points[:, point_index] = new_point
+
+        old_other_point_length = np.linalg.norm(points[:, 1 - point_index])
+
+        other_point = (-1 + (2 * point_index)) * np.array([new_point[1], -new_point[0]])
+        other_point_length = np.linalg.norm(other_point)
+        points[:, 1 - point_index] = other_point / other_point_length
+        points[:, 1 - point_index] *= old_other_point_length
+
+        new_eigenvalues = np.zeros(2, dtype=float)
+        new_eigenvalues[point_index] = new_point_length
+        new_eigenvalues[1 - point_index] = old_other_point_length
+        new_eigenvalues = new_eigenvalues ** 2
+
+        points_norm = points / np.array([np.linalg.norm(points[:, 0]), np.linalg.norm(points[:, 1])])
+
+        cov = points_norm @ (np.eye(2) * new_eigenvalues) @ points_norm.T
+        self.cov = cov
+        self.points = points
---
+++ imuncertain/plotting/interactive_splom.py
@@ -0,0 +1,116 @@
+import matplotlib
+
+from imuncertain.plotting.distribution_plot import InteractiveNormal
+
+matplotlib.use("TkAgg")
+
+
+import numpy as np
+
+from matplotlib.widgets import Button
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+
+class InteractiveSplom:
+    def __init__(self, mean: np.ndarray, cov: np.ndarray, fig_args: dict = None, n_std: float = 1.0,
+                 extends: float = 10, epsilon: float = 5):
+        self.mean = mean
+        self.cov = cov
+
+        self.n_std = n_std
+        self.extends = extends
+        self.epsilon = epsilon
+
+        self.dim = len(mean)
+
+        assert self.dim > 1
+
+        if fig_args is None:
+            fig_args = {}
+        fig, axes = plt.subplots(self.dim-1, self.dim-1, **fig_args)
+        if self.dim <= 2:
+            axes = np.array([axes])
+
+        self.fig = fig
+
+        self.subplots = np.empty(axes.shape, dtype=object)
+
+        for row_i, row in enumerate(axes):
+            row_i += 1
+            for col_i, ax in enumerate(row):
+                if col_i >= row_i:
+                    self.subplots[row_i, col_i] = None
+                    continue
+
+                mean_ij = self.mean[[row_i, col_i]]
+                cov_ij = np.array([[self.cov[col_i, col_i], self.cov[col_i, row_i]],
+                                   [self.cov[row_i, col_i], self.cov[row_i, row_i]]])
+                self.subplots[row_i-1, col_i] = InteractiveNormal(mean_ij, cov_ij, ax, self.n_std,
+                                                                  self.extends, self.epsilon)
+
+        fig.canvas.mpl_connect('button_press_event', self.button_press_callback)
+        fig.canvas.mpl_connect('button_release_event', self.button_release_callback)
+        fig.canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
+
+        self.current_pressed_subplot = None
+
+    def get_current_subplot(self, event):
+        for subplot in self.subplots.flat:
+            if subplot is not None and subplot.ax is event.inaxes:
+                return subplot
+        return None
+
+    def button_press_callback(self, event):
+        'whenever a mouse button is pressed'
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+
+        subplot = self.get_current_subplot(event)
+
+        self.current_pressed_subplot = subplot
+
+        # print(pind)
+        # pind = self.get_ind_under_point(event)
+
+    def button_release_callback(self, event):
+        'whenever a mouse button is released'
+        if event.button != 1:
+            return
+        self.current_pressed_subplot = None
+
+    def motion_notify_callback(self, event):
+        'on mouse movement'
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+
+        new_current_subplot = self.get_current_subplot(event)
+        if new_current_subplot is self.current_pressed_subplot:
+            if new_current_subplot is not None:
+                point_index = new_current_subplot.get_ind_under_point(event)
+                if point_index is not None:
+                    new_current_subplot.adjust_points(point_index, np.array([event.xdata, event.ydata]))
+                    new_current_subplot.update()
+        else:
+            self.current_pressed_subplot = None
+
+    def show(self):
+        self.fig.show()
+
+
+def main():
+    dim = 3
+    mean = np.zeros(dim)
+    cov = np.eye(dim, dim)
+    isplom = InteractiveSplom(mean, cov)
+    isplom.show()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
