5f9fafe483fd4f598a63e04fe1dd30cb6811a727
Marina <eversm@web.de>
2024-05-10 17:49:59 +0200

    Extend README and setup for docs

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# UADAPy
+# UADAPy - Uncertainty-aware Data Analysis with Python
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+TODO: Include image
+
 UADAPy is a Python library to support an easy analysis of uncertain data.
 
+The library provides:
+- uncertainty-aware algorithms for different visualization algorithms, including UAMDS
+- easy-to-use visualizations for uncertain data
+
+## Installation
 So far the library is very much work in progress, but you can already use it via pip:
 ```
 pip install git+https://github.com/hageldave/uadapy
 ```
+
+## Documentation
+You can find the documentation here: 
---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sphinx_rtd_theme
+
+project = 'UADAPy'
+copyright = '2024, Ruben Bauer, Marina Evers, David Hägele, Patrick Paetzold'
+author = 'Ruben Bauer, Marina Evers, David Hägele, Patrick Paetzold'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  "sphinx_rtd_theme",
+  "sphinx.ext.autodoc",
+  "sphinx.ext.napoleon"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
