4f8bc1e14e1370e402e111ad1a487cb57b768a02
hageldave <haegele.david@gmail.com>
2026-06-10 13:48:42 +0200

    student grades dataset as in uapca goertler et al.

---
+++ uadapy/data/student_grades.py
@@ -0,0 +1,164 @@
+import numpy as np
+import scipy.stats as stats
+from collections import namedtuple
+
+def _trapezoid_abcd2cdls(a,b,c,d):
+    """
+    convert trapezoidal parameters a,b,c,d to c',d',location, scale as expected by scipy.stats.trapezoid
+    a = loc
+    d = loc + scale
+    b = loc + c'*scale
+    c = loc + d'*scale
+    """
+    loc = a
+    scale = d - a
+    c_ = (b-a)/scale
+    d_ = (c-a)/scale
+    params = namedtuple('TrapezoidCDLS', ['c', 'd', 'loc', 'scale'])
+    return params(c_,d_,loc,scale)
+
+# define trapezoid parameters for textual grades
+def very_bad():
+    return _trapezoid_abcd2cdls(0, 0, 2, 6)
+
+def bad():
+    return _trapezoid_abcd2cdls(2, 4, 6, 7)
+
+def fairly_bad():
+    return _trapezoid_abcd2cdls(5, 7, 9, 10)
+
+def fairly_good():
+    return _trapezoid_abcd2cdls(10, 11, 13, 14)
+
+def good():
+    return _trapezoid_abcd2cdls(13, 14, 16, 18)
+
+def very_good():
+    return _trapezoid_abcd2cdls(14, 18, 20, 20)
+
+
+# define the students and subjects
+def subject_names():
+    return ['M1', 'M2', 'P1', 'P2']
+
+def student_tom():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: np.ones(n)*15, # M1
+        lambda n, rand_state=None: stats.trapezoid.rvs(*fairly_good(),size=n, random_state=rand_state), # M2
+        lambda n, rand_state=None: stats.norm.rvs(loc=14, scale=5.7, size=n, random_state=rand_state), # P1
+        lambda n, rand_state=None: stats.uniform.rvs(loc=14, scale=2, size=n, random_state=rand_state)  # P2 [14,16]
+    )
+    return s
+
+def student_david():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: np.ones(n)*9, # M1
+        lambda n, rand_state=None: stats.trapezoid.rvs(*good(),size=n, random_state=rand_state), # M2
+        lambda n, rand_state=None: stats.trapezoid.rvs(*fairly_good(),size=n, random_state=rand_state), # P1
+        lambda n, rand_state=None: np.ones(n)*10  # P2
+    )
+    return s
+
+def student_bob():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: np.ones(n)*6, # M1
+        lambda n, rand_state=None: stats.uniform.rvs(loc=10, scale=1, size=n, random_state=rand_state),
+        lambda n, rand_state=None: stats.uniform.rvs(loc=13, scale=7, size=n, random_state=rand_state),
+        lambda n, rand_state=None: stats.trapezoid.rvs(*good(),size=n, random_state=rand_state)
+    )
+    return s
+
+def student_jane():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: stats.trapezoid.rvs(*fairly_good(),size=n, random_state=rand_state),
+        lambda n, rand_state=None: stats.trapezoid.rvs(*very_good(),size=n, random_state=rand_state),
+        lambda n, rand_state=None: np.ones(n)*19,
+        lambda n, rand_state=None: stats.uniform.rvs(loc=10, scale=2, size=n, random_state=rand_state)
+    )
+    return s
+
+def student_joe():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: stats.trapezoid.rvs(*very_bad(),size=n, random_state=rand_state),
+        lambda n, rand_state=None: stats.trapezoid.rvs(*fairly_bad(),size=n, random_state=rand_state),
+        lambda n, rand_state=None: stats.uniform.rvs(loc=10, scale=4, size=n, random_state=rand_state),
+        lambda n, rand_state=None: np.ones(n)*14
+    )
+    return s
+
+def student_jack():
+    samplers = namedtuple('samplers', subject_names())
+    s = samplers(
+        lambda n, rand_state=None: np.ones(n)*1,
+        lambda n, rand_state=None: stats.uniform.rvs(loc=4, scale=2, size=n, random_state=rand_state),
+        lambda n, rand_state=None: np.ones(n)*9,
+        lambda n, rand_state=None: stats.uniform.rvs(loc=6, scale=3, size=n, random_state=rand_state)
+    )
+    return s
+
+
+def students():
+    """
+    Return a dictionary of students and their samplers for each subject
+    """
+    return {
+        'Tom': student_tom(),
+        'David': student_david(),
+        'Bob': student_bob(),
+        'Jane': student_jane(),
+        'Joe': student_joe(),
+        'Jack': student_jack()
+    }
+
+
+def multivariate_sample(student_samplers, n=1, random_state=None):
+    """
+    Given a student's samplers namedtuple, sample n grades for each subject and return a (n,4) array
+
+    Parameters
+    ----------
+    student_samplers : namedtuple
+        A namedtuple containing the samplers for each subject for a student
+    n : int, optional
+        The number of samples to generate for each subject, by default 1
+    random_state : int or np.random.RandomState, optional
+        The random state to use for reproducibility, by default None
+    """
+    M1,M2,P1,P2 = student_samplers
+    return np.array([
+        M1(n, random_state),
+        M2(n, random_state),
+        P1(n, random_state),
+        P2(n, random_state)
+    ]).T
+
+
+def sample_dataset(n_per_stduent=1, random_state=None):
+    """
+    Sample a dataset of grades for all students and subjects
+
+    Parameters
+    ----------
+    n_per_stduent : int, optional
+        The number of samples to generate for each student, by default 1
+    random_state : int or np.random.RandomState, optional
+        The random state to use for reproducibility, by default None
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        A tuple (X, y) where X is a (n_per_student*6, 4) array of grades and y is a (n_per_student*6,) array of corresponding student names
+    """
+    X = []
+    y = []
+    for student_name, samplers in students().items():
+        samples = multivariate_sample(samplers, n=n_per_stduent, random_state=random_state)
+        X.append(samples)
+        y.append(np.array([student_name]*n_per_stduent))
+    return np.vstack(X), np.concatenate(y)
+
